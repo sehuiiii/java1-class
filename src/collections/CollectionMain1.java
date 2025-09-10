959f7a27d65697ec68fc07d855a317c2be05e251
sehuiiii <parksehui.dev@gmail.com>
2025-09-10 20:48:08 +0900

    feat: Update CollectionMain1.java

--- src/collections/CollectionMain1.java
+++ src/collections/CollectionMain1.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 public class CollectionMain1 {
     public static void main(String[] args) {
         ArrayList<String> teacherNames = new ArrayList<>();
-        teacherNames.add("진주");
-        teacherNames.add("도형");
-        teacherNames.add("지영");
+        teacherNames.add("짱구");
+        teacherNames.add("철수");
+        teacherNames.add("영희");
 
 
 //        teacherNames.add(123);
