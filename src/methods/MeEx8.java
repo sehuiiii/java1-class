096be45112f6f5ef8078ed6d61349c0ed745d965
sehuiiii <parksehui.dev@gmail.com>
2025-09-10 20:50:55 +0900

    feat: Update MeEx8.java

--- src/methods/MeEx8.java
+++ src/methods/MeEx8.java
@@ -3,8 +3,8 @@ package methods;
 public class MeEx8 {
     public static void main(String[] args) {
         print("Hello world", 3);
-        print("진주쌤 짱", 5);
-        print("2-4반 최고!", 7);
+        print("짱구보단", 5);
+        print("맹구!", 7);
     }
 
     public static void print(String str, int n) {
