5858cf32570e3043be4955829abb167e13fd1a87
sehuiiii <parksehui.dev@gmail.com>
2025-09-10 20:47:35 +0900

    feat: Update HashMap3.java

--- src/collections/map/HashMap3.java
+++ src/collections/map/HashMap3.java
@@ -8,8 +8,8 @@ import java.util.Map;
 public class HashMap3 {
     public static void main(String[] args) {
         Map<String, List<Integer>> scores = new HashMap<>();
-        scores.put("성한", Arrays.asList(95, 90, 87));
-        scores.put("현우", Arrays.asList(80, 88));
+        scores.put("철수", Arrays.asList(95, 90, 87));
+        scores.put("영희", Arrays.asList(80, 88));
 
         for (String name : scores.keySet()) {
             List<Integer> scoreList = scores.get(name);
